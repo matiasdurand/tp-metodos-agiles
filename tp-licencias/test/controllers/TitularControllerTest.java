2ca449432cf79360f6d5a21688ccee14d87d009c
Matias <matiasdurand@outlook.com>
2019-11-18 21:00:30 -0300

    Se agrega libreria para testing. Se modifican y corrigen los test

--- tp-licencias/test/controllers/TitularControllerTest.java
+++ tp-licencias/test/controllers/TitularControllerTest.java
@@ -1,20 +1,18 @@
-package validators;
+package controllers;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import controllers.TitularController;
 import domain.TypeId;
+
 import dto.TitularDTO;
 
-
-class TitularDataValidatorTest {
+class TitularControllerTest {
 
 	private TitularController titularController = TitularController.getInstance();
 	
@@ -22,44 +20,40 @@ class TitularDataValidatorTest {
 	private TitularDTO titularDTO2 = new TitularDTO();
 	
 	
-	@BeforeEach
-	void testLoadData() {
+	@Test //CASO 1: Datos del titular correctos.
+	void testValidateCorrectTitularData() {
 		
-		//CASO DATOS CORRECTOS
 		titularDTO1.setTypeId(TypeId.DNI);
 		titularDTO1.setPersonalId("40406072");
 		titularDTO1.setName("Matias");
 		titularDTO1.setSurname("Durand");
-		
-		LocalDate b = LocalDate.of(1997, 12, 11);
-		Date birthdate = Date.from(b.atStartOfDay(ZoneId.systemDefault()).toInstant());
-		titularDTO1.setBirthdate(birthdate);
-		
 		titularDTO1.setAdress("Alvear 3483");
 		titularDTO1.setBloodType("A+");
 		titularDTO1.setOrganDonor(false);
 		
-		//CASO DATOS TODOS ERRÓNEOS
+		LocalDate birthdate = LocalDate.of(1997, 12, 11);
+		Date b = Date.from(birthdate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		titularDTO1.setBirthdate(b);
+		
+		assertEquals(0, titularController.validate(titularDTO1).size());
+	}
+	
+	@Test //CASO 2: Datos del titular incorrectos.
+	void testValidateIncorrectTitularData() {
+		
 		titularDTO2.setTypeId(TypeId.DNI);
 		titularDTO2.setPersonalId("A0406072");
 		titularDTO2.setName("");
 		titularDTO2.setSurname("");
-		
-		b = LocalDate.of(2004, 12, 11);
-		birthdate = Date.from(b.atStartOfDay(ZoneId.systemDefault()).toInstant());
-		titularDTO2.setBirthdate(birthdate);
-		
 		titularDTO2.setAdress("");
 		titularDTO2.setBloodType("A+");
 		titularDTO2.setOrganDonor(true);
 		
-	}
-	
-	@Test
-	void testValidate() {
-		assertEquals(0, titularController.validate(titularDTO1).size());
+		LocalDate birthdate = LocalDate.of(2004, 12, 11);
+		Date b = Date.from(birthdate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		titularDTO2.setBirthdate(b);
+		
 		assertEquals(5, titularController.validate(titularDTO2).size());
 	}
 
 }
-
