a7b06515a90c2711604ca1d11a8b3ae98898bb3d
manupa14 <manupa4@live.com.ar>
2019-12-02 18:21:55 -0300

    Se crea metodo para encontrar las ultimas licencisa para titular

--- tp-licencias/src/dao/LicenseDAOSQL.java
+++ tp-licencias/src/dao/LicenseDAOSQL.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -7,6 +8,7 @@ import org.hibernate.SessionFactory;
 import javax.persistence.NoResultException;
 
 import domain.License;
+import domain.Titular;
 
 /**
  * Esta clase es una implementacion para SQL (utilizando Hibernate) de la interfaz LicenseDAO.
@@ -86,5 +88,46 @@ public class LicenseDAOSQL extends GenericDAOSQL<License, Integer> implements Li
 			factory.close();
 	
 		}
+	}
+	
+	
+	public List<License> findLastLicenseOfTitular(int id){
+		//creamos factory
+				SessionFactory factory = createFactory();
+				//creamos session BD
+				Session session = createSession(factory);
+				//creamos consulta HQL
+				try {
+					@SuppressWarnings("unchecked")
+					License LicenciaA = (License) session.createQuery("from Licencia where id_titular = '" + id + "' and clase ='A' and fecha_emision in ( select max(fecha_emision) from Licencia where id_titular= '" + id +"' and clase= 'A')").getSingleResult();
+					License LicenciaB = (License) session.createQuery("from Licencia where id_titular = '" + id + "' and clase ='B' and fecha_emision in ( select max(fecha_emision) from Licencia where id_titular= '" + id +"' and clase= 'B')").getSingleResult();
+					License LicenciaC = (License) session.createQuery("from Licencia where id_titular = '" + id + "' and clase ='C' and fecha_emision in ( select max(fecha_emision) from Licencia where id_titular= '" + id +"' and clase= 'C')").getSingleResult();
+					License LicenciaD = (License) session.createQuery("from Licencia where id_titular = '" + id + "' and clase ='D' and fecha_emision in ( select max(fecha_emision) from Licencia where id_titular= '" + id +"' and clase= 'D')").getSingleResult();
+					License LicenciaE = (License) session.createQuery("from Licencia where id_titular = '" + id + "' and clase ='E' and fecha_emision in ( select max(fecha_emision) from Licencia where id_titular= '" + id +"' and clase= 'E')").getSingleResult();
+					License LicenciaF = (License) session.createQuery("from Licencia where id_titular = '" + id + "' and clase ='F' and fecha_emision in ( select max(fecha_emision) from Licencia where id_titular= '" + id +"' and clase= 'F')").getSingleResult();
+					License LicenciaG = (License) session.createQuery("from Licencia where id_titular = '" + id + "' and clase ='G' and fecha_emision in ( select max(fecha_emision) from Licencia where id_titular= '" + id +"' and clase= 'G')").getSingleResult();
+					
+					List<License> licenses = new ArrayList<License>();
+					licenses.add(LicenciaA);
+					licenses.add(LicenciaB);
+					licenses.add(LicenciaC);
+					licenses.add(LicenciaD);
+					licenses.add(LicenciaE);
+					licenses.add(LicenciaF);
+					licenses.add(LicenciaG);
+					
+					return licenses;
+				}
+				catch(NoResultException e) {
+					return null;
+				}
+				finally {
+					session.getTransaction().commit();
+					session.close();
+					factory.close();
+			
+				}
+		
+		
 	}
 }
